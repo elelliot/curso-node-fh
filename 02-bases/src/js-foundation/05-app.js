c18dbae6494aca727182d1be494fc9eec89c2eec
Elliot Miranda <elliot_150894@hotmail.com>
2025-02-10 11:35:09 -0700

    Updates comments again

--- 02-bases/src/js-foundation/05-app.js
+++ 02-bases/src/js-foundation/05-app.js
@@ -3,7 +3,7 @@ const { getAge, getUUID } = require("./plugins/index");
 const { buildMakePerson } = require("./js-foundation/05-factory");
 
 //Desde aqui le configuro como se crearan el UUID y la edad...
-//... Basicamente, Inyeccion de dependencias
+//... Basicamente, Inyeccion de dependencias (le ando inyectando las dependencias de getUUID y getAge)
 const makePerson = buildMakePerson({ getUUID, getAge });
 
 const obj = { name: "John", birthdate: "1994-10-21" };
