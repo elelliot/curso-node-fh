82d7c61aba8a445daba42ac7b8cab1b3012e8d66
ElElliot <elliot158094@gmail.com>
2025-03-10 20:05:25 -0700

    Updates comments

--- 02-bases/src/plugins/get-age.plugin.ts
+++ 02-bases/src/plugins/get-age.plugin.ts
@@ -6,9 +6,9 @@
 export const getAge = (birthdate: string) => {
     //if(!birthdate) return new Error('Birthdate is required');
     //return getAgePlugin(birthdate);
-    console.log({
-        currentYear: new Date().getFullYear()
-    })
+    // console.log({
+    //     currentYear: new Date().getFullYear()
+    // })
     return new Date().getFullYear() - new Date(birthdate).getFullYear();
 }
 
