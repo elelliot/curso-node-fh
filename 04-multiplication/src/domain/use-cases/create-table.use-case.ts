702442e052992d49c5ef17cc974020593a3e3f95
Elliot Miranda <elliot_150894@hotmail.com>
2025-05-12 13:27:00 -0700

    -Installs @jest/globals, removes @types/jest -Creates create-table-use-case tests.

--- 04-multiplication/src/domain/use-cases/create-table.use-case.ts
+++ 04-multiplication/src/domain/use-cases/create-table.use-case.ts
@@ -5,31 +5,31 @@ Use cases: es que cada uno haga una tarea en particular (Separar las responsabil
 */
 
 export interface CreateTableUseCase {
-    execute: (options: CreateTableOptions) => string;
+  execute: (options: CreateTableOptions) => string;
 }
 
 export interface CreateTableOptions {
-    base: number;
-    limit?: number;
+  base: number;
+  limit?: number;
 }
 
 // 'implements' means the same as an interface, but with a class
 export class CreateTable implements CreateTableUseCase {
-    constructor(
-        /**
-         * DI - Dependency Injection 
-         */
-    ){}
+  constructor() /**
+   * DI - Dependency Injection
+   */ {}
 
+  //solo es una convencion de nombres, no es necesario que se llame execute
+  execute({ base, limit = 10 }: CreateTableOptions) {
+    let outputMessage = "";
 
-    //solo es una convencion de nombres, no es necesario que se llame execute
-    execute({ base, limit = 10 }: CreateTableOptions){
-        let outputMessage = '';
-        
-        for (let i = 1; i <= limit; i++) {
-            outputMessage += `${base} x ${i} = ${base * i}\n`; 
-        }
+    for (let i = 1; i <= limit; i++) {
+      outputMessage += `${base} x ${i} = ${base * i}`; // \n ahora se agrega si no es el ultimo elemento gracias al test
 
-        return outputMessage;
+      //No agregar el salto de linea en el ultimo elemento
+      if (i < limit) outputMessage += "\n";
     }
+
+    return outputMessage;
+  }
 }
