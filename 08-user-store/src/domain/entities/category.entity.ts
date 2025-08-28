fe1f7b8b77c39e3456bd92b0e4d2b3d33bf014ab
Elliot Miranda <elliot_150894@hotmail.com>
2025-08-27 17:02:27 -0700

    Adds comments

--- 08-user-store/src/domain/entities/category.entity.ts
+++ 08-user-store/src/domain/entities/category.entity.ts
@@ -1,5 +1,6 @@
 import { CustomError } from "../errors/custom.error";
 
+// Esto no estuvo en el curso, fue una solucion de la comunidad
 // Por alguna razon los tipos de mongoose no funcionan bien y rompen la app, por eso debo hacer este CategoryEntity, igual es buena practica
 export class CategoryEntity {
   constructor(
